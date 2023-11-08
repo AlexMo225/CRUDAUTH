3ffcf97636dc182d50e575c960c13aa2b60fda40
AlexMo225 <95090160+AlexMo225@users.noreply.github.com>
2023-11-08 13:39:23 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <a href="/registrer">Inscription</a>
+        <a href="/login">Connexion</a>
+    </body>
+</html>
