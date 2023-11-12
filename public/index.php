302e554a5a35fe3c1004d08ee9f9f934f6ea8f22
AlexMo225 <95090160+AlexMo225@users.noreply.github.com>
2023-11-12 23:43:24 +0100

    progess

--- public/index.php
+++ public/index.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <meta name="description" content="">
-        <link rel="stylesheet" href="reset.css">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
     </head>
     <body>
         <a href="/registrer">Inscription</a>
